e54014760e889986ac8550950eeb9f716b502c99
Lucifer1147 <4n0nym0us1147@gmail.com>
2025-01-31 02:39:22 +0530

    Added an input component to make life easire

---
+++ StockSage_Frontend/src/components/inputComponent.svelte
@@ -0,0 +1,18 @@
+<script>
+  let { width, inputType, placeholder, binder = $bindable(), title } = $props();
+  console.log(width, binder)
+</script>
+
+<div
+  class={`flex ${width} h-[10%] rounded-xl text-black text-lg font-semibold font-mono`}
+>
+  <p class="bg-gray-500 rounded-l-xl w-full flex items-center justify-center">
+    {title}
+  </p>
+  <input
+    type={inputType}
+    class="bg-gray-200 outline-none border-none rounded-r-xl w-full"
+    {placeholder}
+    bind:value={binder}
+  />
+</div>
