7ae07053eaac9b8f671e26a0698d54513389d16e
Lucifer1147 <4n0nym0us1147@gmail.com>
2025-02-02 00:20:27 +0530

    Modified to support websockets

--- StockSage_Backend/StockSage_Backend/asgi.py
+++ StockSage_Backend/StockSage_Backend/asgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import main.routing
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StockSage_Backend.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            main.routing.websocket_urlpatterns
+        )
+    )
+})
--- StockSage_Backend/StockSage_Backend/settings.py
+++ StockSage_Backend/StockSage_Backend/settings.py
@@ -31,15 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'main',
     
-    'corsheaders'
+    'channels',
+    'corsheaders',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -72,7 +77,11 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'StockSage_Backend.wsgi.application'
-
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
@@ -84,6 +93,7 @@ DATABASES = {
     }
 }
 
+ASGI_APPLICATION = 'StockSage_Backend.asgi.application'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
