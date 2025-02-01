c11e93ad30dab32b2e58fa9b93479f479a6d1210
Aayush Singla <145104997+lucifer1147@users.noreply.github.com>
2025-02-01 02:45:02 +0000

    Registered 'main' app and added additional allowed host

--- StockSage_Backend/StockSage_Backend/settings.py
+++ StockSage_Backend/StockSage_Backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
     
     'corsheaders'
 ]
@@ -127,4 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # SvelteKit frontend
+    "http://127.0.0.1:5173"
 ]
