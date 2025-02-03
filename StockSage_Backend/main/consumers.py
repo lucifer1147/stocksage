529a8b97e4f6eeb3f4e88c294a1778b0529ff4bd
Lucifer1147 <4n0nym0us1147@gmail.com>
2025-02-03 12:43:25 +0530

    Half backed code for stopping training when ws disconnects. Also temporarily added the training presets

--- StockSage_Backend/main/consumers.py
+++ StockSage_Backend/main/consumers.py
@@ -4,17 +4,98 @@ import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
 from concurrent.futures import ThreadPoolExecutor
 
-from .ProjectFiles.dummyTrain import train
+from .ProjectFiles.train import train
 
 executor = ThreadPoolExecutor(max_workers=1)
 
+param_set_sm = {
+    "tickers": ['NVDA', 'AAPL', 'MSFT', 'AMZN'],
+    "numLayers": 5,
+    "hiddenSize": 512,
+    "learningRate": 0.001,
+    "batchSize": 16,
+    "dropoutProb": 0.2,
+    "addFeatures": [],
+    "timeFrame": 20,
+    "saveExtrasToFile": True,
+    "saveModelAs": "sm_stock_model",
+    "checkpointsIter": 20,
+    "maxEpochs": 50,
+    "minEpochs": 10,
+    "patience": 10,
+    "plotLoss": True,
+}
+
+param_set_md = {
+    "tickers": ['NVDA', 'AAPL', 'MSFT', 'AMZN'],
+    "numLayers": 6,
+    "hiddenSize": 1024,
+    "learningRate": 0.0005,
+    "batchSize": 16,
+    "dropoutProb": 0.25,
+    "addFeatures": ['rsi', 'ema'],
+    "timeFrame": 30,
+    "saveExtrasToFile": True,
+    "saveModelAs": "md_stock_model",
+    "checkpointsIter": 10,
+    "maxEpochs": 100,
+    "minEpochs": 10,
+    "patience": 10,
+    "plotLoss": True,
+}
+
+param_set_lg = {
+    "tickers": ['NVDA', 'AAPL', 'MSFT', 'AMZN'],
+    "numLayers": 8,
+    "hiddenSize": 2048,
+    "learningRate": 0.0001,
+    "batchSize": 32,
+    "dropoutProb": 0.3,
+    "addFeatures": ['macd', 'rsi', 'ema', 'signal line'],
+    "timeFrame": 45,
+    "saveExtrasToFile": True,
+    "saveModelAs": "lg_stock_model",
+    "checkpointsIter": 5,
+    "maxEpochs": 150,
+    "minEpochs": 10,
+    "patience": 10,
+    "plotLoss": True,
+}
+
+param_set_xl = {
+    "tickers": ['NVDA', 'AAPL', 'MSFT', 'AMZN'],
+    "numLayers": 10,
+    "hiddenSize": 4096,
+    "learningRate": 0.00001,
+    "batchSize": 64,
+    "dropoutProb": 0.4,
+    "addFeatures": ['macd', 'rsi', 'ema', 'signal line', 'fast%k', 'slow%d'],
+    "timeFrame": 60,
+    "saveExtrasToFile": True,
+    "saveModelAs": "xl_stock_model",
+    "checkpointsIter": 2,
+    "maxEpochs": 200,
+    "minEpochs": 10,
+    "patience": 10,
+    "plotLoss": True,
+}
+
+PRESETS = {
+    'sm': param_set_sm,
+    'md': param_set_md,
+    'lg': param_set_lg,
+    'xl': param_set_xl
+}
+
 class TrainingConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.stop_training = False
         await self.accept()
 
     async def receive(self, text_data):
         data = json.loads(text_data)
         params = data.get("params", {})
+        params = PRESETS[params.get("preset", "sm")]
         
         loop = asyncio.get_event_loop()
 
@@ -28,8 +109,12 @@ class TrainingConsumer(AsyncWebsocketConsumer):
             import asyncio
             asyncio.run_coroutine_threadsafe(send_update(message), loop)
             
+        def train_with_stop_signal():
+            train(callback=callback, stopSignal=lambda: self.stop_training, **params)
+            
         # Offload the train function to a separate thread
-        await loop.run_in_executor(executor, lambda: train(**params, callback=callback))
+        await loop.run_in_executor(executor, train_with_stop_signal)
 
     async def disconnect(self, close_code):
+        self.stop_training = True  # Trigger to stop the training
         pass
